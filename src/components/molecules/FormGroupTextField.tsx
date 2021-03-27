61517a84a72fbf9ba6b92feae453fc7bfb1dbc40
Hiroaki Date <mmst3l4@gmail.com>
2021-03-27 14:49:38 +0900

    FormGroupTextFieldコンポーネントを実装

---
+++ src/components/molecules/FormGroupTextField.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import { TextField } from '@/components/atoms/TextField';
+
+export interface FormGroupTextFieldProps {
+  text: string;
+  placeholder: string;
+  handleChangeText: (value: string) => void;
+  disable: boolean;
+  labelText: string;
+}
+
+export const FormGroupTextField: React.FC<FormGroupTextFieldProps> = ({text, placeholder, handleChangeText, disable, labelText}) => {
+
+  return (
+    <FormGroupTextFieldWrapper>
+      <LabelWrapper>
+        <Label>{labelText}</Label>
+      </LabelWrapper>
+      
+      <TextField
+        text={text}
+        placeholder={placeholder} 
+        handleChangeText={handleChangeText}
+        disable={disable}
+      />
+    </FormGroupTextFieldWrapper>
+  )
+};
+
+const Label = styled.label`
+  font-size: 16px;
+  line-height: 23px;
+  color: #31313D;
+`;
+
+const LabelWrapper = styled.div`
+  margin-bottom: 14px;
+`;
+
+const FormGroupTextFieldWrapper = styled.div`
+  display: inline-block;
+  width: 100%;
+`;
---
+++ src/stories/molecules/FormGroupTextField.stories.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { FormGroupTextField, FormGroupTextFieldProps } from '@/components/molecules/FormGroupTextField';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+export default {
+  title: 'molecules',
+  component: FormGroupTextField,
+} as Meta;
+
+export const FormGroupTextFieldWrapper: Story<FormGroupTextFieldProps> = (args) => {
+  const [text, setText] = useState('');
+
+  return <FormGroupTextField {...args} text={text} handleChangeText={setText} />
+};
+
+FormGroupTextFieldWrapper.args = {
+  placeholder: 'placeholder',
+  disable: false,
+  labelText: '項目名'
+};
+
+FormGroupTextFieldWrapper.storyName = 'FormGroupTextField';
