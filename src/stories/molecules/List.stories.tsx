177c6dca0f878779d71e26e25d95855372763e9d
Hiroaki Date <mmst3l4@gmail.com>
2021-03-28 17:24:24 +0900

    storybook上のパス修正

--- src/stories/molecules/List.stories.tsx
+++ src/stories/molecules/List.stories.tsx
@@ -3,7 +3,7 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 import { List, ListProps } from '@/components/molecules/List';
 
 export default {
-  title: 'atoms',
+  title: 'molecules',
   component: List,
 } as Meta;
 
